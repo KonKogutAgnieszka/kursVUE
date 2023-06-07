da7dc4a737f3955483139eb768750896fd769414
Agnieszka Koń-Kogut <konagnieszka@gmail.com>
2023-06-07 08:41:05 +0200

    section2, event binding

--- basics-03-events-starting-code/basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/basics-03-events-starting-code/app.js
@@ -2,8 +2,27 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: "",
+      confirmedName: "",
     };
+  },
+  methods: {
+    confirmInput() {
+      this.name = confirmedName;
+    },
+    submitForm(event) {
+      alert("Submitted");
+    },
+    add(number) {
+      this.counter += number;
+    },
+    remove(number) {
+      this.counter -= number;
+    },
+    setName(event, lastName) {
+      this.name = event.target.value + " " + lastName;
+    },
   },
 });
 
-app.mount('#events');
+app.mount("#events");
