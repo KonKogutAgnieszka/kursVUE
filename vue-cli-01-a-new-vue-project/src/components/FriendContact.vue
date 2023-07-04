3dec0c85cbaead52431fcd9cddd5c0dc5353aa46
Agnieszka Koń-Kogut <konagnieszka@gmail.com>
2023-07-04 08:52:13 +0200

    section 7: Moving to a Better Development Setup & Vue CLI

---
+++ vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,28 @@
+<template>
+    <li>
+        <h2>{{ friend.name }}</h2>
+        <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+        <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong> {{ friend.phone }} </li>
+            <li><strong>Email:</strong> {{ friend.mail}}</li>
+        </ul>
+    </li>
+</template>
+
+<script>
+export default {
+data(){
+    return{
+    detailsAreVisible: false,
+    friend: {
+        id: 'konik',
+        name: 'Agnieszka Koń',
+        phone: '723941103',
+        mail: 'konagnieszka@gmail.com'
+        }
+    }},
+methods:{
+    toggleDetails(){
+        this.detailsAreVisible = !this.detailsAreVisible;
+    }
+}}</script>
