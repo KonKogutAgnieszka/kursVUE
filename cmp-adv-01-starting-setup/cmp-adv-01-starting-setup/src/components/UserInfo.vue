c7b812b7e5595436bcac6ad5b3b9c332983b1e38
Agnieszka Koń-Kogut <konagnieszka@gmail.com>
2023-08-22 09:54:13 +0200

    section 9:slots and components

--- cmp-adv-01-starting-setup/cmp-adv-01-starting-setup/src/components/UserInfo.vue
+++ cmp-adv-01-starting-setup/cmp-adv-01-starting-setup/src/components/UserInfo.vue
@@ -1,9 +1,11 @@
 <template>
   <section>
-    <div>
+    <base-card>
+    <template #header>
       <h3>{{ fullName }}</h3>
       <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
-    </div>
+    </template>
+    </base-card>
     <p>{{ infoText }}</p>
   </section>
 </template>
@@ -14,18 +16,3 @@ export default {
 };
 </script>
 
-<style scoped>
-section {
-  margin: 2rem auto;
-  max-width: 30rem;
-  border-radius: 12px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  padding: 1rem;
-}
-
-section header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-</style>
