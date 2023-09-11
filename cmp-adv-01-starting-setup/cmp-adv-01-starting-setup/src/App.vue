9f2ff654b2316d89ba77c9f288a238fdeab3bf4e
Agnieszka Koń-Kogut <konagnieszka@gmail.com>
2023-09-11 08:54:24 +0200

    section 9: fragments, components, teleporting

--- cmp-adv-01-starting-setup/cmp-adv-01-starting-setup/src/App.vue
+++ cmp-adv-01-starting-setup/cmp-adv-01-starting-setup/src/App.vue
@@ -3,9 +3,9 @@
     <the-header></the-header>
     <button @click="setSelectedComponent('active-goals')">Active Goals</button>
     <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
-    <active-goals v-if="selectedComponent === 'active-goals'"></active-goals>
-    <manage-goals v-if="selectedComponent === 'manage-goals'"></manage-goals>
+    <keep-alive>
     <component :is="selectedComponent"></component>
+  </keep-alive>
   </div>
 </template>
 
