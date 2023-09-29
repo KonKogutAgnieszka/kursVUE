afe879f97c2faeb5e0ee0c89789aaad44d07bb3b
Agnieszka Koń-Kogut <konagnieszka@gmail.com>
2023-09-29 09:01:43 +0200

    section 10: learning resources app initial commit

---
+++ prj-cmp-01-starting-setup/src/components/App.vue
@@ -0,0 +1,54 @@
+<template>
+    <the-header title="RememberMe">
+        <stored-resources :resources="storedResources"></stored-resources>
+    </the-header>
+  </template>
+  
+  <script>
+  import storedResources from './learning-resources/StoredResources.vue';
+  import TheHeader from './layouts/TheHeader.vue';
+  
+  export default {
+    components: {
+        storedResources,
+        TheHeader
+    },
+    data() {
+      return {
+        storedResources: [
+          {
+            id: 'official-guide',
+            title: 'Official Guide',
+            description: 'The official Vue.js documentation',
+            link: 'https://vuejs.org'
+          },
+          {
+            id: 'google',
+            title: 'Google',
+            description: 'Learn to google...',
+            link: 'https://google.com'
+          },
+        ]
+      };
+    }
+  };
+  </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
+
+
+  
